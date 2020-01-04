107ad3eb2b4e1c72f824e56d2ffe21de95f3dbe8
gaurav386i <yadavgaurav386@gmail.com>
2020-01-04 19:30:48 +0530

    added local time support using flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -18,6 +20,8 @@ login = LoginManager(app)
 mail = Mail(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 """Email setup for sending error logs via email"""
 
